f477981efefdcce18269475c45abe0340ff86907
PritamP20 <pripritam7@gmail.com>
2025-04-28 06:51:49 +0530

    qr added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
+function App() {
+  const ExternalRedirect = () => {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScGC99O2W0OL0BkZ1pSxt06Ou6-jaIM9Rcq_zkFGeehg2Lp6g/viewform";
+    return null;
+  };
+  return (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
@@ -17,11 +22,14 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/qr" element={<ExternalRedirect/>}></Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
+}
 
 export default App;
